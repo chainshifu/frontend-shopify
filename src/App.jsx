5be91a3864b87903be264bff7b87de7f15be48dc
dalton <dalton9592@gmail.com>
2024-01-30 01:26:25 +0630

    update Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import Slider from "react-slick";
+import Headphone from "../../assets/images/Wireless-Headphone.png";
+import Virtual from "../../assets/images/vr.png";
+import Macbook from "../../assets/images/macbook-pro.png";
+import Button from "./Button";
+
+const HeroData = [
+  {
+    id: 1,
+    image: Headphone,
+    subtitle: "Beats Solo",
+    title: "Wireless",
+    title_1: "Headphone",
+  },
+  {
+    id: 2,
+    image: Virtual,
+    subtitle: "Beats Solo",
+    title: "Wireless",
+    title_1: "Virtual",
+  },
+  {
+    id: 3,
+    image: Macbook,
+    subtitle: "Beats Solo",
+    title: "Wireless",
+    title_1: "Laptops",
+  },
+];
+
+function Hero() {
+  const settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    //   autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
+  };
+  return (
+    <section className="container">
+      <div className="hero overflow-hidden min-h-[550px] sm:min-h-[650px] rounded-3xl flex justify-center items-center">
+        <div className="container pb-8 sm:pb-0">
+          <Slider {...settings}>
+            {HeroData.map((data) => (
+              <div key={data.id}>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                  <div className="flex flex-col justify-center gap-4 sm:pl-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                    <h1 className="text-2xl sm:text-3xl lg:text-2xl font-bold">
+                      {data.subtitle}
+                    </h1>
+                    <h1 className="text-4xl sm:text-4xl lg:text-6xl font-bold">
+                      {data.title}
+                    </h1>
+                    <h1 className="text-4xl sm:text-[60px] md:text-[80px] xl:text-[130px] uppercase font-bold text-white dark:text-white/5">
+                      {data.title_1}
+                    </h1>
+                    <div>
+                      <Button
+                        text="Shop By Categories"
+                        bgColor="bg-primary"
+                        textColor="text-white"
+                      />
+                    </div>
+                  </div>
+                  <div className="order-1 sm:order-2">
+                    <div>
+                      <img
+                        src={data.image}
+                        alt=""
+                        className="
+                    w-[300px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,0.4)] relative z-40
+                    "
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
