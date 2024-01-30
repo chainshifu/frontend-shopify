0c5e278d0b85cd3f3d09b9f325c5c1652e575424
dalton <dalton9592@gmail.com>
2024-01-31 01:07:29 +0630

    added the Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,17 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/Services/Services";
 
-const BannerData = [
-  {
-    discount: "30% OFF",
-    title: "Fine Smile",
-    date: "10 Jan to 28 Jan",
-    image: "https://pngfre.com/wp-content/uploads/Headphone-1-781x1024.png",
-    subtitle: "Air Solo Bass",
-    title_2: "Winter Sale",
-    description:
-      "When you listen to music, you want to the truest possible representation of the artist real sound. Enjoy crip, powerful sound and dramatic noice reduction.",
-    bgColor: "#f42c37",
-  },
-];
+const BannerData = {
+  discount: "30% OFF",
+  title: "Fine Smile",
+  date: "10 Jan to 28 Jan",
+  image: "https://pngfre.com/wp-content/uploads/Headphone-1-781x1024.png",
+  subtitle: "Air Solo Bass",
+  title_2: "Winter Sale",
+  description:
+    "When you listen to music, you want to the truest possible representation of the artist real sound. Enjoy crip, powerful sound and dramatic noice reduction.",
+  bgColor: "#f42c37",
+};
 
 function App() {
   return (
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,16 +2,37 @@ import React from "react";
 
 function Banner({ bannerData }) {
   return (
-    <div className="min-h-[550px] flex justify-center items-center py-10">
+    <div className="min-h-[550px] flex justify-center items-center pt-5 pb-6">
       <div className="container">
         <div
           style={{ backgroundColor: bannerData.bgColor }}
-          className="grid grid-cols-1 md:grid-cols-3 gap-6"
+          className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl"
         >
-          <div>
-            <p>{bannerData.discount}</p>
-            <h1>{bannerData.title}</h1>
-            <p>{bannerData.date}</p>
+          <div className="p-6 sm:p-8">
+            <p className="font-medium">{bannerData.discount}</p>
+            <h1 className="text-4xl lg:text-6xl uppercase font-bold">
+              {bannerData.title}
+            </h1>
+            <p className="text-sm">{bannerData.date}</p>
+          </div>
+          <div className="h-full flex items-center">
+            <img
+              src={bannerData.image}
+              alt=""
+              className="scale-105 w-[250px] md:w-[330px] mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,0.4)] object-cover"
+            />
+          </div>
+          <div className="flex flex-col gap-4 p-6 sm:p-5">
+            <p className="text-xl font-bold">{bannerData.subtitle}</p>
+            <p className="text-3xl sm:text-4xl font-bold">
+              {bannerData.title_2}
+            </p>
+            <p className="text-sm tracking-wide leading-5">
+              {bannerData.description}
+            </p>
+            <button className="bg-white text-primary w-[150px] py-2 px-4 rounded-md font-medium">
+              Shop Now
+            </button>
           </div>
         </div>
       </div>
