4447b6cde301533c54e922314e191840c04fbc17
dalton <dalton9592@gmail.com>
2024-01-31 05:38:34 +0630

    reused Banner Section

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Services from "./components/Services/Services";
 import Headphone from "./assets/images/Headphone_banner.png";
+import Watch from "./assets/images/apple_watch_banner_2.png";
 import Product from "./components/Product.jsx/Product";
+import Banner2 from "./components/Banner/Banner2";
 
 const BannerData = {
   discount: "30% OFF",
@@ -20,6 +22,18 @@ const BannerData = {
   bgColor: "#f42c37",
 };
 
+const BannerData_2 = {
+  discount: "25% OFF",
+  title: "Happy Hours",
+  date: "10 Jan to 28 Jan",
+  image: Watch,
+  subtitle: "Smart Solo",
+  title_2: "Winter Sale",
+  description:
+    "Measure your blood oxygen level. Keep an eye on your heart. See your fitness metrics on the enchanced Always-On Retina display. Live a healthier, more active, more connected life.",
+  bgColor: "#2dcc6f",
+};
+
 function App() {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white overflow-hidden">
@@ -30,6 +44,7 @@ function App() {
       <Services />
       <Banner bannerData={BannerData} />
       <Product />
+      <Banner2 bannerData={BannerData_2} />
     </div>
   );
 }
---
+++ src/components/Banner/Banner2.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Banner2({ bannerData }) {
+  return (
+    <div className="min-h-[550px] flex justify-center items-center pt-0 pb-6">
+      <div className="container">
+        <div
+          style={{ backgroundColor: bannerData.bgColor }}
+          className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl"
+        >
+          <div className="p-6 sm:p-8">
+            <p className="font-medium">{bannerData.discount}</p>
+            <h1 className="text-4xl lg:text-6xl uppercase font-bold">
+              {bannerData.title}
+            </h1>
+            <p className="text-sm">{bannerData.date}</p>
+          </div>
+          <div className="h-full flex items-center">
+            <img
+              src={bannerData.image}
+              alt=""
+              className="scale-125 w-[250px] md:w-[330px] mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,0.4)] object-cover"
+            />
+          </div>
+          <div className="flex flex-col gap-4 p-6 sm:p-5">
+            <p className="text-xl font-bold">{bannerData.subtitle}</p>
+            <p className="text-3xl sm:text-4xl font-bold">
+              {bannerData.title_2}
+            </p>
+            <p className="text-sm tracking-wide leading-5">
+              {bannerData.description}
+            </p>
+            <button className="bg-white text-bgGreen w-[150px] py-2 px-4 rounded-md font-medium">
+              Shop Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner2;
