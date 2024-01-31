a5a393e28ab625390cf9c906283c6798b44d2560
dalton <dalton9592@gmail.com>
2024-02-01 01:07:24 +0630

    Custom OrderPopup Section

--- src/components/PopUp/OrderPopup.jsx
+++ src/components/PopUp/OrderPopup.jsx
@@ -8,7 +8,7 @@ function OrderPopup({ order, handleOrderPopup }) {
       {order && (
         <div>
           <div className="h-screen w-screen fixed top-0 left-0 bg-black/50 z-50 backdrop-blur-sm">
-            <div className="w-[400px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 rounded-xl">
+            <div className="w-[300px] sm:w-[400px] sm:h-[300px] lg:w-[450px] fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 rounded-xl">
               <div className="flex items-center justify-between">
                 <h1>Order Now</h1>
                 <div>
