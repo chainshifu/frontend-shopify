bdac802ee8432384f447d8d5cfc175c8725a8bff
dalton <dalton9592@gmail.com>
2024-02-01 02:05:59 +0630

    some update and animated Banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -9,13 +9,20 @@ function Banner({ bannerData, handleOrderPopup }) {
           className="grid grid-cols-1 md:grid-cols-3 gap-6 items-center text-white rounded-3xl"
         >
           <div className="p-6 sm:p-8">
-            <p className="font-medium">{bannerData.discount}</p>
-            <h1 className="text-4xl lg:text-6xl uppercase font-bold">
+            <p data-aos="slide-right" className="font-medium">
+              {bannerData.discount}
+            </p>
+            <h1
+              data-aos="zoom-out"
+              className="text-4xl lg:text-6xl uppercase font-bold"
+            >
               {bannerData.title}
             </h1>
-            <p className="text-sm">{bannerData.date}</p>
+            <p data-aos="fade-up" className="text-sm">
+              {bannerData.date}
+            </p>
           </div>
-          <div className="h-full flex items-center">
+          <div data-aos="zoom-in" className="h-full flex items-center">
             <img
               src={bannerData.image}
               alt=""
@@ -23,19 +30,23 @@ function Banner({ bannerData, handleOrderPopup }) {
             />
           </div>
           <div className="flex flex-col gap-4 p-6 sm:p-5">
-            <p className="text-xl font-bold">{bannerData.subtitle}</p>
-            <p className="text-3xl sm:text-4xl font-bold">
+            <p data-aos="zoom-out" className="text-xl font-bold">
+              {bannerData.subtitle}
+            </p>
+            <p data-aos="fade-up" className="text-3xl sm:text-4xl font-bold">
               {bannerData.title_2}
             </p>
-            <p className="text-sm tracking-wide leading-5">
+            <p data-aos="fade-up" className="text-sm tracking-wide leading-5">
               {bannerData.description}
             </p>
-            <button
-              className="bg-white text-primary w-[150px] py-2 px-4 rounded-md font-medium"
-              onClick={handleOrderPopup}
-            >
-              Shop Now
-            </button>
+            <div data-aos="fade-up" data-aos-offset="0">
+              <button
+                className="bg-white text-primary w-[150px] py-2 px-4 rounded-md font-medium"
+                onClick={handleOrderPopup}
+              >
+                Shop Now
+              </button>
+            </div>
           </div>
         </div>
       </div>
